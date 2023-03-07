d20d8f3ac1de7506328159f1151027a37e415de5
AdyatmaNugrahaYudha <awan2403@gmail.com>
2023-03-07 09:11:16 +0700

    Add multiple bonus shapes

--- worksheet1/gl.js
+++ worksheet1/gl.js
@@ -64,7 +64,6 @@ var theta = 0.0;
 var thetaLoc;
 
 window.addEventListener("load", (event) => {
-    console.log("page is fully loaded");
     init();
 });
 
@@ -132,8 +131,44 @@ function init() {
         first = true;
     }
 
-    document.getElementById("other").onclick = function () {
-        mode = 4;
+//todo: add stuff
+    document.getElementById("random-square").onclick = function () {
+        mode = 5;
+        animate = false;
+        tmp = [];
+        first = true;
+    }
+
+    document.getElementById("plus").onclick = function () {
+        mode = 6;
+        animate = false;
+        tmp = [];
+        first = true;
+    }
+
+    document.getElementById("4-leaf").onclick = function () {
+        mode = 7;
+        animate = false;
+        tmp = [];
+        first = true;
+    }
+
+    document.getElementById("hexagon").onclick = function () {
+        mode = 8;
+        animate = false;
+        tmp = [];
+        first = true;
+    }
+
+    document.getElementById("5-leaf").onclick = function () {
+        mode = 9;
+        animate = false;
+        tmp = [];
+        first = true;
+    }
+
+    document.getElementById("compass").onclick = function () {
+        mode = 10;
         animate = false;
         tmp = [];
         first = true;
@@ -201,7 +236,164 @@ function init() {
         var point = vec2(clickX, clickY);
 
         if (first) {
-            if (mode != 99) {
+            if (mode == 5) {
+                for (var i = 0; i < 4; i++) {
+                    var k = generateRandomNumber();
+                    var l = generateRandomNumber();
+                    tmp.push(vec2(point[0] + k, point[1] + l));
+                }
+                for (var i = 0; i < 4; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (index + i), flatten(tmp[i]));
+                index += 4;
+                tmp = [];
+                gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+                var tt = colors;
+                for (var i = 0; i < 4; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index - 4 + i), flatten(tt));
+                shapes.push(4);
+            } else if (mode == 6) {
+                tmp.push(point);
+                tmp.push(vec2(point[0] - 0.025, point[1] + 0.075));
+                tmp.push(vec2(point[0] + 0.025, point[1] + 0.075));
+
+                tmp.push(vec2(point[0] + 0.025, point[1] + 0.025));
+                tmp.push(vec2(point[0] + 0.075, point[1] + 0.025));
+
+                tmp.push(vec2(point[0] + 0.075, point[1] - 0.025));
+                tmp.push(vec2(point[0] + 0.025, point[1] - 0.025));
+
+                tmp.push(vec2(point[0] + 0.025, point[1] - 0.075));
+                tmp.push(vec2(point[0] - 0.025, point[1] - 0.075));
+
+                tmp.push(vec2(point[0] - 0.025, point[1] - 0.025));
+                tmp.push(vec2(point[0] - 0.075, point[1] - 0.025));
+
+                tmp.push(vec2(point[0] - 0.075, point[1] + 0.025));
+                tmp.push(vec2(point[0] - 0.025, point[1] + 0.025));
+
+                tmp.push(vec2(point[0] - 0.025, point[1] + 0.075));
+                tmp.push(vec2(point[0] + 0.025, point[1] + 0.075));
+
+                for (var i = 0; i < 14; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (index + i), flatten(tmp[i]));
+                tmp = [];
+                gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+                var tt = colors;
+                for (var i = 0; i < 14; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index + i), flatten(tt));
+                index += 14;
+                shapes.push(6);
+
+            } else if (mode == 7) {
+                tmp.push(point);
+                tmp.push(vec2(point[0], point[1] + 0.1));
+                tmp.push(vec2(point[0] + 0.025, point[1] + 0.025));
+
+                tmp.push(vec2(point[0] + 0.1, point[1]));
+                tmp.push(vec2(point[0] + 0.025, point[1] - 0.025));
+
+                tmp.push(vec2(point[0], point[1] - 0.1));
+                tmp.push(vec2(point[0] - 0.025, point[1] - 0.025));
+
+                tmp.push(vec2(point[0] - 0.1, point[1]));
+                tmp.push(vec2(point[0] - 0.025, point[1] + 0.025));
+
+                tmp.push(vec2(point[0], point[1] + 0.1));
+
+                for (var i = 0; i < 10; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (index + i), flatten(tmp[i]));
+                tmp = [];
+                gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+                var tt = colors;
+                for (var i = 0; i < 10; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index + i), flatten(tt));
+                index += 10;
+                shapes.push(7);
+
+            } else if (mode == 8) {
+                tmp.push(point);
+                tmp.push(vec2(point[0], point[1] + 0.1));
+                tmp.push(vec2(point[0] + 0.07, point[1] + 0.070));
+
+                tmp.push(vec2(point[0] + 0.1, point[1]));
+                tmp.push(vec2(point[0] + 0.07, point[1] - 0.07));
+
+                tmp.push(vec2(point[0], point[1] - 0.1));
+                tmp.push(vec2(point[0] - 0.07, point[1] - 0.07));
+
+                tmp.push(vec2(point[0] - 0.1, point[1]));
+                tmp.push(vec2(point[0] - 0.07, point[1] + 0.07));
+
+                tmp.push(vec2(point[0], point[1] + 0.1));
+
+                for (var i = 0; i < 10; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (index + i), flatten(tmp[i]));
+                tmp = [];
+                gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+                var tt = colors;
+                for (var i = 0; i < 10; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index + i), flatten(tt));
+                index += 10;
+                shapes.push(8);
+
+            } else if (mode == 9) {
+                tmp.push(point);
+                //create 12 points for 5-pinted-star
+                tmp.push(vec2(point[0], point[1] + 0.1));
+                tmp.push(vec2(point[0] + 0.023, point[1] + 0.028));
+
+                tmp.push(vec2(point[0] + 0.08, point[1] + 0.033));
+                tmp.push(vec2(point[0] + 0.033, point[1] - 0.012));
+
+                tmp.push(vec2(point[0] + 0.05, point[1] - 0.08));
+                tmp.push(vec2(point[0], point[1] - 0.033));
+
+                tmp.push(vec2(point[0] - 0.05, point[1] - 0.08));
+                tmp.push(vec2(point[0] - 0.033, point[1] - 0.012));
+
+                tmp.push(vec2(point[0] - 0.08, point[1] + 0.033));
+                tmp.push(vec2(point[0] - 0.023, point[1] + 0.028));
+
+                tmp.push(vec2(point[0], point[1] + 0.1));
+
+                for (var i = 0; i < 12; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (index + i), flatten(tmp[i]));
+                tmp = [];
+                gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+                var tt = colors;
+                for (var i = 0; i < 12; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index + i), flatten(tt));
+                index += 12;
+                shapes.push(9);
+
+            } else if (mode == 10) {
+                tmp.push(point);
+
+                tmp.push(vec2(point[0], point[1] + 0.1));
+                tmp.push(vec2(point[0] + 0.025, point[1] + 0.05));
+
+                tmp.push(vec2(point[0] + 0.05, point[1] + 0.05));
+                tmp.push(vec2(point[0] + 0.05, point[1] + 0.025));
+
+                tmp.push(vec2(point[0] + 0.1, point[1]));
+                tmp.push(vec2(point[0] + 0.05, point[1] - 0.025));
+
+                tmp.push(vec2(point[0] + 0.05, point[1] - 0.05));
+                tmp.push(vec2(point[0] + 0.025, point[1] - 0.05));
+
+                tmp.push(vec2(point[0], point[1] - 0.1));
+                tmp.push(vec2(point[0] - 0.025, point[1] - 0.05));
+
+                tmp.push(vec2(point[0] - 0.05, point[1] - 0.05));
+                tmp.push(vec2(point[0] - 0.05, point[1] - 0.025));
+
+                tmp.push(vec2(point[0] - 0.1, point[1]));
+                tmp.push(vec2(point[0] - 0.05, point[1] + 0.025));
+
+                tmp.push(vec2(point[0] - 0.05, point[1] + 0.05));
+                tmp.push(vec2(point[0] - 0.025, point[1] + 0.05));
+
+                tmp.push(vec2(point[0], point[1] + 0.1));
+
+                for (var i = 0; i < 18; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 8 * (index + i), flatten(tmp[i]));
+                tmp = [];
+                gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+                var tt = colors;
+                for (var i = 0; i < 18; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index + i), flatten(tt));
+                index += 18;
+                shapes.push(10);
+
+            } else if (mode != 99) {
                 first = false;
                 tmp[0] = point;
             }
@@ -248,13 +440,20 @@ function init() {
                 var tt = colors;
                 for (var i = 0; i < 4; i++) gl.bufferSubData(gl.ARRAY_BUFFER, 16 * (index - 4 + i), flatten(tt));
                 shapes.push(4);
-            } else if (mode == 0) {
-
             }
         }
     });
+
     render();
 }
+
+function generateRandomNumber() {
+    var min = 0.00,
+        max = 0.1,
+        highlightedNumber = Math.random() * (max - min) + min;
+
+    return (highlightedNumber);
+};
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -265,12 +464,6 @@ function render() {
         theta += 0.1;
         console.log("theta: " + theta);
         gl.uniform1f(thetaLoc, theta);
-
-        // var s = Math.sin(theta);
-        // var c = Math.cos(theta);
-        //
-        // xCal = -s*yCal + c*xCal;
-        // yCal =  s*xCal + c*yCal;
     }
 
     for (var i = 0; i < shapes.length; i += 1) {
@@ -287,6 +480,27 @@ function render() {
             gl.drawArrays(gl.TRIANGLE_FAN, count, poly[polyCount]);
             count+= poly[polyCount];
             polyCount++;
+        } else if (shapes[i] == 5) {
+            gl.drawArrays(gl.TRIANGLE_FAN, count, 4);
+            count+= 4;
+        } else if (shapes[i] == 6) {
+            gl.drawArrays(gl.TRIANGLE_FAN, count, 14);
+            count+= 14;
+        } else if (shapes[i] == 7) {
+            gl.drawArrays(gl.TRIANGLE_FAN, count, 10);
+            count+= 10;
+        } else if (shapes[i] == 7) {
+            gl.drawArrays(gl.TRIANGLE_FAN, count, 10);
+            count += 10;
+        } else if (shapes[i] == 8) {
+            gl.drawArrays(gl.TRIANGLE_FAN, count, 10);
+            count += 10;
+        } else if (shapes[i] == 9) {
+            gl.drawArrays(gl.TRIANGLE_FAN, count, 12);
+            count += 12;
+        } else if (shapes[i] == 10) {
+            gl.drawArrays(gl.TRIANGLE_FAN, count, 18);
+            count += 18;
         }
     }
     requestAnimationFrame(render);
